afa0f68ecf109dd39aa15f6d59693e9c55de2c8d
Abdelati Elasri <abdelattie@gmail.com>
2025-07-23 17:24:22 +0100

    feat: implement PostgreSQL database connection and health checks

--- services/auth/go.mod
+++ services/auth/go.mod
@@ -1,3 +1,5 @@
 module example/hello
 
 go 1.24.5
+
+require github.com/lib/pq v1.10.9 // indirect
--- services/auth/main.go
+++ services/auth/main.go
@@ -1,19 +1,64 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	_ "github.com/lib/pq"
 )
 
+var db *sql.DB
+
 func main() {
-    http.HandleFunc("/", HelloServer)
-    fmt.Println("Auth API server running at http://localhost:8081/\nClick the URL to test the welcome message.")
-    http.ListenAndServe(":8081", nil)
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		dbHost, dbPort, dbUser, dbPassword, dbName)
+
+	var err error
+	db, err = sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Successfully connected to database!")
+	http.HandleFunc("/", HelloServer)
+	http.HandleFunc("/healthz", healthzHandler)
+	http.HandleFunc("/readyz", readyzHandler)
+
+	fmt.Println("Auth API server running at http://localhost:8080/")
+	http.ListenAndServe(":8080", nil)
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello at Auth!")
+	fmt.Fprintf(w, "Hello at Auth!")
 }
 
+func healthzHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
+func readyzHandler(w http.ResponseWriter, r *http.Request) {
+	err := db.Ping()
+	if err != nil {
+		http.Error(w, "db not ready", http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
 
 // Hello world example from https://yourbasic.org/golang/http-server-example/
