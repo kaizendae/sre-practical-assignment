a50e50e63d4c0cf26352e7bb0eb79f7eb7b369c1
Abdelati Elasri <abdelattie@gmail.com>
2025-07-23 17:38:01 +0100

    chore: add liveness and readiness endpints api serivce add dotenv and pg dependencies

--- services/api/index.js
+++ services/api/index.js
@@ -1,9 +1,34 @@
+require('dotenv').config();
 const express = require('express');
+const { Pool } = require('pg');
+
 const app = express();
-const port = 8082;
+const port = 3000;
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
 
 app.get('/', (req, res) => {
   res.send('Hello from Node.js API service!');
+});
+
+app.get('/healthz', (req, res) => {
+  res.status(200).send('OK');
+});
+
+app.get('/readyz', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    client.release();
+    res.status(200).send('OK');
+  } catch (err) {
+    res.status(500).send('db not ready');
+  }
 });
 
 app.listen(port, () => {
