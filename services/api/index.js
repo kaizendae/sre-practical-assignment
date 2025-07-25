e556ccfcd990e8ab1f4dea316cc478e318725c73
Abdelati <a.elasri@elham.sa>
2025-07-25 16:14:06 +0100

    feat: integrate Prometheus metrics middleware and update dependencies

--- services/api/index.js
+++ services/api/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const promBundle = require("express-prom-bundle");
 const { Pool } = require('pg');
 
 const app = express();
@@ -13,9 +14,36 @@ const pool = new Pool({
   database: process.env.DB_NAME,
 });
 
-app.get('/', (req, res) => {
-  res.send('Hello from Node.js API service!');
+// Add the options to the prometheus middleware most option are for http_request_duration_seconds histogram metric
+const metricsMiddleware = promBundle({
+ includeMethod: true, 
+ includePath: true, 
+ includeStatusCode: true, 
+ includeUp: true,
+ customLabels: {project_name: 'hello_world', project_type: 'test_metrics_labels'},
+ promClient: {
+ collectDefaultMetrics: {
+ }
+ }
 });
+
+// add the prometheus middleware to all routes
+app.use(metricsMiddleware)
+
+app.get('/', (req, res) => {
+  res.send(`
+    <h1>Node.js API Service</h1>
+    <p>Welcome! Here are the available endpoints:</p>
+    <ul>
+      <li><b>GET <a href="/">/</a></b> - Show this directory</li>
+      <li><b>GET <a href="/healthz">/healthz</a></b> - Liveness probe, returns 200 OK if service is up</li>
+      <li><b>GET <a href="/readyz">/readyz</a></b> - Readiness probe, returns 200 OK if DB is ready, 500 otherwise</li>
+      <li><b>GET <a href="/metrics">/metrics</a></b> - Prometheus metrics endpoint</li>
+    </ul>
+  `);
+});
+
+
 
 app.get('/healthz', (req, res) => {
   res.status(200).send('OK');
