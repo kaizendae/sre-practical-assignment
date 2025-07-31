44ec19156a07ca44305bfba606ff3a8c49513d2c
Abdelati Elasri <abdelattie@gmail.com>
2025-07-31 14:37:27 +0100

    fix: debug connectivity issues between api service and otel-collector

--- services/api/instrumentation.js
+++ services/api/instrumentation.js
@@ -1,22 +1,28 @@
 /*instrumentation.js*/
 // Require dependencies
+const { diag, DiagConsoleLogger, DiagLogLevel } = require('@opentelemetry/api');
 const { NodeSDK } = require('@opentelemetry/sdk-node');
 const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http');
 const { PrometheusExporter } = require('@opentelemetry/exporter-prometheus');
+const { Resource } = require('@opentelemetry/resources');
+const {
+  SemanticResourceAttributes,
+} = require('@opentelemetry/semantic-conventions');
 const {
   getNodeAutoInstrumentations,
 } = require('@opentelemetry/auto-instrumentations-node');
-const {
-  PeriodicExportingMetricReader,
-} = require('@opentelemetry/sdk-metrics');
+
+diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
 
 const sdk = new NodeSDK({
+  resource: new Resource({
+    [SemanticResourceAttributes.SERVICE_NAME]: 'dice-server',
+    [SemanticResourceAttributes.SERVICE_VERSION]: '0.1.0',
+  }),
   traceExporter: new OTLPTraceExporter({
-    url: process.env.OTEL_EXPORTER_OTLP_ENDPOINT,
-    headers: {
-      'x-otlp-project-name': 'nodejs-api-service',
-      'x-otlp-project-type': 'test_metrics_labels',
-    },
+    url:
+      process.env.OTEL_EXPORTER_OTLP_ENDPOINT || 'http://localhost:4318/v1/traces',
+    headers: {}, // Add any headers if needed
   }),
   metricReader: new PrometheusExporter({
     startServer: true,
